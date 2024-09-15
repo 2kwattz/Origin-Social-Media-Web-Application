0c71a65616114982ea9b9194bf88bc81cd9d18cf
Roshan Bhatia <prakashbhatia1970@gmail.com>
2024-09-15 15:48:45 +0530

    Refinements in Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/general/header/header";
+import styles from "./App.module.css"
 
 function App() {
 
     return <React.Fragment>
 
         <Header />
+        <div className={styles.bodyWrapper}>
+
+
+        </div>
         
       
     </React.Fragment>
--- client/src/components/general/header/header.jsx
+++ client/src/components/general/header/header.jsx
@@ -5,6 +5,7 @@ import iconHome from '../../../assets/img/icons/Home.png';
 import chatIcon from '../../../assets/img/icons/Chat.png'
 import NotificationsIcon from '../../../assets/img/icons/Notifications.png'
 import ProfileIcon from '../../../assets/img/icons/Profile.png'
+import ShopIcon from '../../../assets/img/icons/shop.png'
 
 function Header() {
 
@@ -19,7 +20,7 @@ function Header() {
             <ul className={styles.navbarListItems}>
                 <li className={styles.navbarListItem}><a href="/"><img src={iconHome} height="27px" width="27px"/></a></li>
                 <li className={styles.navbarListItem}><a href="/about"><img src={chatIcon} style={{marginTop: 4}}  height="19px" width="19px"/></a></li>
-                <li className={styles.navbarListItem}><a href="/account/login"><img src="/img/icons/Shop.png" height="20px" width="20px"/></a></li>
+                <li className={styles.navbarListItem}><a href="/account/login"><img src={ShopIcon} height="20px" width="20px"/></a></li>
                 <li className={styles.navbarListItem}><a href="/community/chat"><img src={NotificationsIcon} height="27px" width="27px"/></a></li>
                 <li className={styles.navbarListItem}><a href="/blog/writeblog"><img src={ProfileIcon} height="20px" width="20px"/></a></li>
                 <li className={styles.navbarListItem}><button className={styles.btnBlackMed}>Get Started</button></li>
